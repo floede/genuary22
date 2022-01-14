b04357b1f56d822445f19d77a3dbfca1b5acba03
Janus Hasseriis <janus@hasseriis.net>
2022-01-14 18:13:33 +0100

    Day 9

--- 09/sketch.js
+++ 09/sketch.js
@@ -14,33 +14,80 @@ function setup() {
 
 function building1(left, height, width, col) {
   noStroke();
-  //strokeWeight(strokeWidth);
-  //stroke(colors[Math.round(random(9))].rgb);
   fill(col);
   rect(left, h - height, width, height); //  x y width height
   triangle(
     left,
     h - height,
     left + width / 2,
-    height - width / 2,
+    h - height - width / 2,
     width + left,
-    height
+    h - height
   );
 }
 
 function building2(left, height, width, col) {
   noStroke();
-  //strokeWeight(strokeWidth);
-  //stroke(colors[Math.round(random(9))].rgb);
   fill(col);
   rect(left, h - height, width, height); //  x y width height
-  arc(left + width / 2, height, width, width, PI, TWO_PI); // x y width height
+  arc(left + width / 2, h - height, width, width, PI, TWO_PI); // x y width height
+}
+
+function building3(left, height, width, col) {
+  noStroke();
+  fill(col);
+  rect(left, h - height, width, height); //  x y width height
+}
+
+function building4(left, height, width, col) {
+  noStroke();
+  fill(col);
+  rect(left, h - height, width, height);
+  rect(left + 20, h - height - 20, width - 40, 20); //  x y width height
+}
+
+function building5(left, height, width, col) {
+  noStroke();
+  fill(col);
+  rect(left, h - height, width, height);
+  triangle(
+    left,
+    h - height,
+    left + width,
+    h - height - width / 2,
+    width + left,
+    h - height
+  );
 }
 
 function draw() {
   strokeCap(SQUARE);
-  building2(buildingLeftX - 2, buildingHeight, buildingWidth + 4, 0);
-  building2(buildingLeftX, buildingHeight, buildingWidth, 255);
+  //building4(buildingLeftX - 2, buildingHeight, buildingWidth + 4, 0);
+  //building5(buildingLeftX, buildingHeight, buildingWidth, 255);
+  for (let row = 0; row < 7; row++) {
+    for (let col = 0; col < 5; col++) {
+      let rand = Math.round(random(5));
+      let left = buildingLeftX + col * 100 + random(50) - 25;
+      let height = buildingHeight - row * 60 - random(20) - 10;
+      let width = buildingWidth + random(40) - 20;
+      let color = 200 - 20 * row;
+      if (rand === 1) {
+        building1(left, height, width, color);
+      }
+      if (rand === 2) {
+        building2(left, height, width, color);
+      }
+      if (rand === 3) {
+        building3(left, height, width, color);
+      }
+      if (rand === 4) {
+        building4(left, height, width, color);
+      }
+      if (rand === 5) {
+        building5(left, height, width, color);
+      }
+    }
+  }
   noLoop();
-  //saveCanvas(c, "Architecture test", "jpg");
+  saveCanvas(c, "Architecture", "jpg");
 }
