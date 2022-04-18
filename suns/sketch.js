11bc38f6bbff571b3d4478d704c4f0849c19cc4d
Janus Hasseriis <janus@hasseriis.net>
2022-04-18 21:52:20 +0200

    Sun-like pattern inspired by book of circles

--- suns/sketch.js
+++ suns/sketch.js
@@ -1,10 +1,62 @@
 const w = 1000;
 const h = 1000;
 
+const cx = w * 0.5;
+const cy = h * 0.5;
+const pokeW = w * 0.01;
+const pokeH = h * 0.11;
+
+const radius = w * 0.3;
+
+let x, y;
+
 function setup() {
   c = createCanvas(w, h);
   colorMode(HSB);
-  background(80);
+  background(100);
 }
 
-function draw() {}
+function draw() {
+  const num = 120;
+
+  const rings = 4;
+  const ringWidth = 100 / rings;
+  const variation = 40;
+
+  for (let reps = 0; reps < rings; reps++) {
+    let col = colors[0][Math.floor(random(0, 10))].hsb;
+    rectMode(CENTER);
+    if (reps === rings - 1) {
+      rectMode(CORNER);
+    }
+    for (let index = 0; index < num; index++) {
+      const slice = degToRad(360 / num);
+      const angle = slice * index;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      push();
+      translate(x, y);
+      rotate(-angle);
+      noStroke();
+      fill(col);
+
+      if (random() > 0.15) {
+        rect(
+          -pokeW * 0.5,
+          -pokeH * 0.1 +
+            (reps === rings - 1
+              ? -pokeH * 2.2
+              : -2.2 * pokeH - (rings - reps) * 1.1 * pokeH),
+          pokeW,
+          random(-variation, variation) + pokeH
+        );
+      }
+
+      pop();
+    }
+  }
+
+  noLoop();
+}
