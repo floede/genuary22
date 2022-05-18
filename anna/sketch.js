c8b720c004f7cfcb527d45714919dee6bf8de756
Janus Hasseriis <janus.hasseriis@tryg.dk>
2022-05-18 17:28:58 +0200

    Changes to Anna

--- anna/sketch.js
+++ anna/sketch.js
@@ -1,12 +1,13 @@
 const w = 1000;
 const h = w;
 
-const squareW = 50;
-const squareH = 75;
 const border = 25;
 
-let noOfCols = Math.round((w - 2 * border) / squareW);
-let noOfRows = Math.round((h - 2 * border) / squareH);
+let noOfCols = 10; //Math.floor((w - 2 * border) / squareW);
+let noOfRows = 7; //Math.floor((h - 2 * border) / squareH);
+
+const squareW = (w - 2 * border) / noOfCols;
+const squareH = (h - 2 * border) / noOfRows;
 
 let extraBorderW = 0.5 * (w - squareW * noOfCols);
 let extraBorderH = 0.5 * (h - squareH * noOfRows);
@@ -15,7 +16,8 @@ let coords = [];
 let elements = [];
 
 function setup() {
-  c = createCanvas(w, w);
+  c = createCanvas(w, w, WEBGL);
+  background(255);
 
   for (var j = 0; j < noOfRows; j++) {
     let row = [];
@@ -30,7 +32,7 @@ function setup() {
 }
 
 function draw() {
-  stroke(120);
+  translate(-w / 2, -h / 2);
   for (let j = 0; j < coords.length; j++) {
     for (let i = 0; i < coords[j].length; i++) {
       elements.push(new BaseElement(coords[j][i].x, coords[j][i].y));
@@ -43,13 +45,33 @@ class BaseElement {
   constructor(x, y) {
     this.x = x;
     this.y = y;
+
     this.x1 = x;
     this.x2 = x + squareW / 2;
     this.x3 = x + squareW;
+
     this.y1 = y;
-    this.y2 = x;
-    this.y3 = x;
-    rect(x, y, squareW, squareH);
+    this.y2 = y + squareH / 2;
+    this.y3 = y + squareH;
+
+    //rect(x, y, squareW, squareH);
+    noStroke();
+    fill(random(255));
     triangle(this.x1, this.y1, this.x2, this.y1, this.x1, this.y2);
+    fill(random(255));
+    triangle(this.x2, this.y1, this.x2, this.y2, this.x1, this.y2);
+    fill(random(255));
+    triangle(this.x2, this.y1, this.x3, this.y1, this.x3, this.y2);
+    fill(random(255));
+    triangle(this.x2, this.y1, this.x2, this.y2, this.x3, this.y2);
+
+    fill(random(255));
+    triangle(this.x1, this.y2, this.x2, this.y3, this.x1, this.y3);
+    fill(random(255));
+    triangle(this.x1, this.y2, this.x2, this.y2, this.x2, this.y3);
+    fill(random(255));
+    triangle(this.x2, this.y2, this.x3, this.y2, this.x2, this.y3);
+    fill(random(255));
+    triangle(this.x3, this.y2, this.x3, this.y3, this.x2, this.y3);
   }
 }
