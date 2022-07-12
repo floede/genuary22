1aac818f91183a9143235ebf4d2f6d28ac4b2815
Janus Hasseriis <janus.hasseriis@tryg.dk>
2022-07-12 19:59:11 +0200

    Adding shades

--- anna/sketch.js
+++ anna/sketch.js
@@ -1,30 +1,45 @@
 const w = 1000;
 const h = w;
 
-const border = 25;
+const border = w / 20;
 
-let noOfCols = 10; //Math.floor((w - 2 * border) / squareW);
-let noOfRows = 7; //Math.floor((h - 2 * border) / squareH);
+let noOfCols = 6; //Math.floor((w - 2 * border) / squareW);
+let noOfRows = 4; //Math.floor((h - 2 * border) / squareH);
 
 const squareW = (w - 2 * border) / noOfCols;
 const squareH = (h - 2 * border) / noOfRows;
 
-let extraBorderW = 0.5 * (w - squareW * noOfCols);
-let extraBorderH = 0.5 * (h - squareH * noOfRows);
+const unit = w / 1000;
 
 let coords = [];
 let elements = [];
+let shadows = [];
+
+const pattern = [
+  [
+    [95, 60, 70],
+    [100, 0, 10],
+    [20, 20, 80],
+    [100, 0, 90],
+    [0, 0, 90],
+    [20, 20, 80],
+    [100, 0, 10],
+    [95, 60, 70],
+  ],
+  [20, 80, 20, 80, 20, 80, 20, 80],
+];
 
 function setup() {
   c = createCanvas(w, w, WEBGL);
-  background(255);
+  colorMode(HSB);
+  background(95, 60, 70);
 
   for (var j = 0; j < noOfRows; j++) {
     let row = [];
     for (var i = 0; i < noOfCols; i++) {
       row.push({
-        x: border + extraBorderW + i * squareW,
-        y: border + extraBorderH + j * squareH,
+        x: border + i * squareW,
+        y: border + j * squareH,
       });
     }
     coords.push(row);
@@ -35,14 +50,37 @@ function draw() {
   translate(-w / 2, -h / 2);
   for (let j = 0; j < coords.length; j++) {
     for (let i = 0; i < coords[j].length; i++) {
-      elements.push(new BaseElement(coords[j][i].x, coords[j][i].y));
+      elements.push(new BaseElement(coords[j][i].x, coords[j][i].y, j, i));
+      if (j === 0) {
+        shadows.push(new ShadowLine([coords[j][i].x], "vertical"));
+        shadows.push(
+          new ShadowLine([coords[j][i].x + squareW / 2], "vertical")
+        );
+      }
+      if (i === 0) {
+        shadows.push(new ShadowLine([coords[j][i].y], "horizontal"));
+        shadows.push(
+          new ShadowLine([coords[j][i].y + squareH / 2], "horizontal")
+        );
+      }
     }
   }
+  elements.forEach((element) => {
+    element.build(pattern);
+  });
+
+  shadows.push(new ShadowLine([height - border], "horizontal"));
+  shadows.push(new ShadowLine([width - border], "vertical"));
+
+  shadows.forEach((element) => {
+    element.build();
+  });
+
   noLoop();
 }
 
 class BaseElement {
-  constructor(x, y) {
+  constructor(x, y, row, col) {
     this.x = x;
     this.y = y;
 
@@ -54,24 +92,100 @@ class BaseElement {
     this.y2 = y + squareH / 2;
     this.y3 = y + squareH;
 
-    //rect(x, y, squareW, squareH);
-    noStroke();
-    fill(random(255));
-    triangle(this.x1, this.y1, this.x2, this.y1, this.x1, this.y2);
-    fill(random(255));
-    triangle(this.x2, this.y1, this.x2, this.y2, this.x1, this.y2);
-    fill(random(255));
-    triangle(this.x2, this.y1, this.x3, this.y1, this.x3, this.y2);
-    fill(random(255));
-    triangle(this.x2, this.y1, this.x2, this.y2, this.x3, this.y2);
+    this.row = row;
+    this.col = col;
 
-    fill(random(255));
-    triangle(this.x1, this.y2, this.x2, this.y3, this.x1, this.y3);
-    fill(random(255));
-    triangle(this.x1, this.y2, this.x2, this.y2, this.x2, this.y3);
-    fill(random(255));
-    triangle(this.x2, this.y2, this.x3, this.y2, this.x2, this.y3);
-    fill(random(255));
-    triangle(this.x3, this.y2, this.x3, this.y3, this.x2, this.y3);
+    this.triangles = [];
+
+    this.triangles.push([this.x1, this.y1, this.x2, this.y1, this.x1, this.y2]);
+    this.triangles.push([this.x2, this.y1, this.x2, this.y2, this.x1, this.y2]);
+    this.triangles.push([this.x2, this.y1, this.x2, this.y2, this.x3, this.y2]);
+    this.triangles.push([this.x2, this.y1, this.x3, this.y1, this.x3, this.y2]);
+    this.triangles.push([this.x1, this.y2, this.x2, this.y3, this.x1, this.y3]);
+    this.triangles.push([this.x1, this.y2, this.x2, this.y2, this.x2, this.y3]);
+    this.triangles.push([this.x2, this.y2, this.x3, this.y2, this.x2, this.y3]);
+    this.triangles.push([this.x3, this.y2, this.x3, this.y3, this.x2, this.y3]);
+  }
+  build(pattern) {
+    for (let i = 0; i < this.triangles.length; i++) {
+      this.show(this.triangles[i], pattern[0][i]);
+    }
+  }
+  show(coords, color) {
+    noStroke();
+    fill(color);
+    triangle(...coords);
+    shadows.push(new ShadowLine([this.x2, this.y1, this.x3, this.y2], "cross"));
+    shadows.push(new ShadowLine([this.x2, this.y1, this.x1, this.y2], "cross"));
+    shadows.push(new ShadowLine([this.x1, this.y2, this.x2, this.y3], "cross"));
+    shadows.push(new ShadowLine([this.x3, this.y2, this.x2, this.y3], "cross"));
+  }
+}
+
+class ShadowLine {
+  constructor(pos, direction) {
+    this.pos1 = pos[0];
+    this.direction = direction;
+
+    this.lightShade = [0, 0.01];
+    this.darkShade = [0, 0.02];
+
+    this.lines = [];
+
+    strokeWeight(unit);
+
+    if (this.direction === "horizontal") {
+      this.lines.push({
+        coords: [0, this.pos1 - unit, width, this.pos1 - unit],
+        color: this.lightShade,
+      });
+
+      this.lines.push({
+        coords: [0, this.pos1, width, this.pos1],
+        color: this.darkShade,
+      });
+
+      this.lines.push({
+        coords: [0, this.pos1 + unit, width, this.pos1 + unit],
+        color: this.lightShade,
+      });
+    } else if (this.direction === "vertical") {
+      this.lines.push({
+        coords: [this.pos1 - unit, 0, this.pos1 - unit, height],
+        color: this.lightShade,
+      });
+      this.lines.push({
+        coords: [this.pos1, 0, this.pos1, height],
+        color: this.darkShade,
+      });
+      this.lines.push({
+        coords: [this.pos1 + unit, 0, this.pos1 + unit, height],
+        color: this.lightShade,
+      });
+    } else {
+      this.lines.push({
+        coords: [pos[0] - unit, pos[1], pos[2] - unit, pos[3]],
+        color: this.lightShade,
+      });
+      this.lines.push({
+        coords: pos,
+        color: this.darkShade,
+      });
+      this.lines.push({
+        coords: [pos[0] + unit, pos[1], pos[2] + unit, pos[3]],
+        color: this.lightShade,
+      });
+    }
+  }
+  build() {
+    for (let i = 0; i < this.lines.length; i++) {
+      this.show(this.lines[i]);
+    }
+  }
+  show(elm) {
+    push();
+    stroke(...elm.color);
+    line(...elm.coords);
+    pop();
   }
 }
